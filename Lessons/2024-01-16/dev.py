ea2c1812a47465914120ce77389d3e03711613f5
Cody Tessler <codytessler@gmail.com>
2024-01-15 20:19:15 -0500

    Removed dev.py

--- Lessons/2024-01-16/dev.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Can I have an x please?" ))
-
-if (x > 0):
-    print(f"{x} is positive!")
-elif x == 0:
-    print(f"Good compromise! {x} is neither positive nor negative")
-else:
-    print(f"fine... {x} is negative...")
-    
